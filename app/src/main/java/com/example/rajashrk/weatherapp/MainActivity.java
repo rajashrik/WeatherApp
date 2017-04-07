ca720d3148ca9447cf44456b8cfd4241fe26dceb
Rajashri <jadhavrajashri49@gmail.com>
2017-04-07 18:17:35 +0530

    create basic list view

--- app/src/main/java/com/example/rajashrk/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/rajashrk/weatherapp/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.rajashrk.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] cityArray = {"Hyderabd","Mumbai","Delhi"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item,cityArray);
+
+        ListView view = (ListView) findViewById(R.id.city_list);
+        view.setAdapter(adapter);
     }
+
 }
