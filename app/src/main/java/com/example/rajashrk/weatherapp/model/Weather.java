97a040f8c72af6c183e7384f1c15260353ff3f44
jhansireddy <jhansireddy007@gmail.com>
2017-04-09 12:52:37 +0530

    Added Weather Model

---
+++ app/src/main/java/com/example/rajashrk/weatherapp/model/Weather.java
@@ -0,0 +1,119 @@
+package com.example.rajashrk.weatherapp.model;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by jhansirk on 09/04/17.
+ */
+public class Weather {
+
+    private String name;
+    private int visibility;
+    private List<WeatherDetails> weather;
+    private Temperature main;
+    private Wind wind;
+    private Sys sys;
+
+    public String getName() {
+        return name;
+    }
+
+    public List<WeatherDetails> getWeather() {
+        return weather;
+    }
+
+    public int getVisibility() {
+        return visibility;
+    }
+
+    public Temperature getMain() {
+        return main;
+    }
+
+    public Wind getWind() {
+        return wind;
+    }
+
+    public Sys getSys() {
+        return sys;
+    }
+
+    public class WeatherDetails {
+        private String description;
+        private String icon;
+        private int id;
+        private String main;
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getIcon() {
+            return icon;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getMain() {
+            return main;
+        }
+    }
+
+    public class Temperature {
+        private float humidity;
+        private float pressure;
+        private float temp;
+        private float temp_max;
+        private float temp_min;
+
+        public float getHumidity() {
+            return humidity;
+        }
+
+        public float getPressure() {
+            return pressure;
+        }
+
+        public float getTemp() {
+            return temp;
+        }
+
+        public float getTemp_max() {
+            return temp_max;
+        }
+
+        public float getTemp_min() {
+            return temp_min;
+        }
+    }
+
+    public class Wind {
+        private float deg;
+        private float speed;
+
+        public float getDeg() {
+            return deg;
+        }
+
+        public float getSpeed() {
+            return speed;
+        }
+    }
+
+    public class Sys {
+        private long sunrise;
+        private long sunset;
+
+        public long getSunrise() {
+            return sunrise;
+        }
+
+        public long getSunset() {
+            return sunset;
+        }
+    }
+}
