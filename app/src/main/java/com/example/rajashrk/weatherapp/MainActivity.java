6cba04bba1c9cd2bbb00bce48a61ea645e249625
JyotsnaGorle <jyotsnagorle@gmail.com>
2017-04-18 00:30:17 +0530

    Add weather status image on landing screen [Jyo]

--- app/src/main/java/com/example/rajashrk/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/rajashrk/weatherapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.example.rajashrk.weatherapp.model.Weather;
@@ -19,19 +20,29 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements WeatherResponseListener {
 
     private  Weather currentWeather = null;
+    private Map<String, Integer> weatherStatusImageMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setWeatherStatusImageMap();
         showWeatherOfCurrentCity();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    private void setWeatherStatusImageMap() {
+        weatherStatusImageMap.put("light rain",R.drawable.rainy_main);
+        weatherStatusImageMap.put("moderate rain",R.drawable.sun);
+        weatherStatusImageMap.put("clear sky",R.drawable.sunny_main);
     }
 
     public void fetchForecast(View view) {
@@ -74,6 +85,8 @@ public class MainActivity extends AppCompatActivity implements WeatherResponseLi
         TextView temperature  = (TextView) findViewById(R.id.temperature);
         temperature.setText(presenter.getTemperatureInCelsius());
 
+        ImageView weatherImage = (ImageView) findViewById(R.id.weatherImage);
+        weatherImage.setImageResource(weatherStatusImageMap.get(currentWeather.getWeather().get(0).getDescription()));
 
         String weekday_name = new SimpleDateFormat("EEEE", Locale.ENGLISH).format(System.currentTimeMillis());
         TextView date  = (TextView) findViewById(R.id.date);
