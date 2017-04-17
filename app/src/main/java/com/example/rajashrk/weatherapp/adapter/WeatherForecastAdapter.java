8f61d18e75a61ea8b330798f73259ea3a50dc9e9
jyotsna <jyotsna@injyotsna.local>
2017-04-17 15:02:45 +0530

    Add weather forecast icons in the list[Jyo]

--- app/src/main/java/com/example/rajashrk/weatherapp/adapter/WeatherForecastAdapter.java
+++ app/src/main/java/com/example/rajashrk/weatherapp/adapter/WeatherForecastAdapter.java
@@ -4,13 +4,17 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Interpolator;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.rajashrk.weatherapp.R;
 import com.example.rajashrk.weatherapp.model.WeatherForecast;
 import com.example.rajashrk.weatherapp.presenter.WeatherForecastPresenter;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by rajashrk on 4/12/17.
@@ -20,11 +24,19 @@ public class WeatherForecastAdapter extends BaseAdapter {
     private Context context;
     private LayoutInflater inflater;
     private List<WeatherForecast> weatherList;
+    private Map<String, Integer> weatherIconsList = new HashMap<>();
 
     public WeatherForecastAdapter(Context context, List<WeatherForecast> weatherList) {
         this.context = context;
         inflater = LayoutInflater.from(context);
         this.weatherList = weatherList;
+        constructweatherIconMap();
+    }
+
+    private void constructweatherIconMap() {
+        weatherIconsList.put("light rain",R.drawable.cloud);
+        weatherIconsList.put("moderate rain",R.drawable.sun);
+        weatherIconsList.put("sky is clear",R.drawable.cloud);
     }
 
 
@@ -57,6 +69,7 @@ public class WeatherForecastAdapter extends BaseAdapter {
         TextView temperature_max;
         TextView temperature_min;
         TextView up_arrow;
+        ImageView weatherImg;
 
     }
 
@@ -67,7 +80,12 @@ public class WeatherForecastAdapter extends BaseAdapter {
         viewHolder.temperature_max.setText(presenter.getTemperatureMaxIncelcius());
         viewHolder.temperature_min.setText(presenter.getTemperatureMinIncelcius());
         viewHolder.up_arrow.setText(R.string.upArrow);
+        viewHolder.weatherImg.setImageResource(getWeatherIcon(weatherList.get(position).getWeather().getDescription()));
 
+    }
+
+    private int getWeatherIcon(String weatherStatus) {
+        return weatherIconsList.get(weatherStatus);
     }
 
     private View createView() {
@@ -77,7 +95,7 @@ public class WeatherForecastAdapter extends BaseAdapter {
         viewHolder.temperature_max = (TextView) view.findViewById(R.id.temperature_max);
         viewHolder.temperature_min = (TextView) view.findViewById(R.id.temperature_min);
         viewHolder.up_arrow = (TextView) view.findViewById(R.id.temperature_max_arrow);
-
+        viewHolder.weatherImg = (ImageView) view.findViewById(R.id.weatherimg);
         view.setTag(viewHolder);
         return view;
     }
